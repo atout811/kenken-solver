e0fe0a3dad3889c355bbf007980a979cba1c0a4e
atout811 <dmcgfx811@gmail.com>
2022-06-01 15:25:29 +0200

    create CSP Class

---
+++ csp.py
@@ -0,0 +1,15 @@
+class CSP():
+   
+    def __init__(self, variables, domains, neighbors, constraints):
+        variables = variables or list(domains.keys())
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.initial = ()
+        self.curr_domains = None
+        self.nassigns = 0
+    
+    
+
+
